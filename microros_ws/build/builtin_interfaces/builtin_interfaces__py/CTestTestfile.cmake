040004374b86e17851ebb843c6879aae56e7ada7
DianYueGuo <dianyueguo@gmail.com>
2024-12-23 16:30:57 +0000

    remove microros_ws/

--- microros_ws/build/builtin_interfaces/builtin_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /SAUVC2024-RPI/microros_ws/build/builtin_interfaces/builtin_interfaces__py
-# Build directory: /SAUVC2024-RPI/microros_ws/build/builtin_interfaces/builtin_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- microros_ws/build/micro_ros_agent/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/SAUVC2024-RPI/microros_ws/install/builtin_interfaces/share/builtin_interfaces/package.sh"
-. "/SAUVC2024-RPI/microros_ws/install/micro_ros_msgs/share/micro_ros_msgs/package.sh"
