e14f54fc44e5a3ff705f813aa915321ac663f47e
DianYueGuo <dianyueguo@gmail.com>
2024-12-23 16:23:10 +0000

    Reset repository to the state of commit ecd2e0fc9d13994ffe722d12fa76e368211e2e9e

---
+++ microros_ws/build/builtin_interfaces/builtin_interfaces__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /SAUVC2024-RPI/microros_ws/build/builtin_interfaces/builtin_interfaces__py
+# Build directory: /SAUVC2024-RPI/microros_ws/build/builtin_interfaces/builtin_interfaces__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
