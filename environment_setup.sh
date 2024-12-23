485f3845e7b97ee2215dcd42d7d08354dd0a8e64
DianYueGuo <dianyueguo@gmail.com>
2024-12-24 00:59:52 +0800

    add volumn mounting -v //Users/joseph/AUV-Projects/SAUVC2024-RPI/microros_ws:/microros_ws to environment_setup.sh

--- environment_setup.sh
+++ environment_setup.sh
@@ -7,5 +7,5 @@ if [ "$(docker ps -a --format '{{.Names}}' | grep -w $CONTAINER_NAME)" ]; then
   docker start -ai $CONTAINER_NAME
 else
   echo "Creating and starting a new container: $CONTAINER_NAME"
-  docker run -it --net=host -v /dev:/dev --privileged --name $CONTAINER_NAME ros:humble
+  docker run -it --net=host -v /dev:/dev -v /$PWD/microros_ws:/microros_ws --privileged --name $CONTAINER_NAME ros:humble
 fi
