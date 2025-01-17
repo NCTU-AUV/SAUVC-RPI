ea40aea2721740df333123df0be1abce4a2203bb
DianYueGuo <dianyueguo@gmail.com>
2025-01-17 17:14:53 +0800

    add initialize_thruster action

--- orca_auv_rpi_ros2_ws/src/orca_auv_thruster_pkg/orca_auv_thruster_pkg/thruster_controller_node.py
+++ orca_auv_rpi_ros2_ws/src/orca_auv_thruster_pkg/orca_auv_thruster_pkg/thruster_controller_node.py
@@ -3,12 +3,28 @@
 
 import rclpy
 from rclpy.node import Node
+from rclpy.action import ActionServer
+
+from orca_auv_thruster_interfaces_pkg.action import InitializeThrusterAction
 
 
 class ThrusterControllerNode(Node):
 
     def __init__(self):
         super().__init__("thruster_controller_node", namespace="orca_auv")
+
+        self.__initialize_thruster_action_servers = [
+            ActionServer(
+                self,
+                InitializeThrusterAction,
+                f"thruster_{thruster_number}/initialize_thruster",
+                lambda goal_handle: self.__execute_callback_with_thruster_number(goal_handle, thruster_number)
+            )
+            for thruster_number in range(8)
+        ]
+
+    def __execute_callback_with_thruster_number(self, goal_handle, thruster_number):
+        pass
 
 
 def main(args=None):
