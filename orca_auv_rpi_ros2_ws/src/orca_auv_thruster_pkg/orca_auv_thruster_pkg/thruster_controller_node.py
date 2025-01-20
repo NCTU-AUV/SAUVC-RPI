f32d49c79f15f67fd87132a4230de170f811bac9
DianYueGuo <dianyueguo@gmail.com>
2025-01-20 18:07:15 +0800

    add set_pwm_output_on and set_pwm_output_signal_value_us publisher

--- orca_auv_rpi_ros2_ws/src/orca_auv_thruster_pkg/orca_auv_thruster_pkg/thruster_controller_node.py
+++ orca_auv_rpi_ros2_ws/src/orca_auv_thruster_pkg/orca_auv_thruster_pkg/thruster_controller_node.py
@@ -4,6 +4,8 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionServer
+from std_msgs.msg import Bool
+from std_msgs.msg import Int32
 
 from orca_auv_thruster_interfaces_pkg.action import InitializeThrusterAction
 
@@ -12,6 +14,24 @@ class ThrusterControllerNode(Node):
 
     def __init__(self):
         super().__init__("thruster_controller_node", namespace="orca_auv")
+
+        self.__set_pwm_output_on_value_publishers = [
+            self.create_publisher(
+                msg_type=Bool,
+                topic=f"thruster_{thruster_number}/set_pwm_output_on",
+                qos_profile=10
+            )
+            for thruster_number in range(8)
+        ]
+
+        self.__set_pwm_output_signal_value_publishers = [
+            self.create_publisher(
+                msg_type=Int32,
+                topic=f"thruster_{thruster_number}/set_pwm_output_signal_value_us",
+                qos_profile=10
+            )
+            for thruster_number in range(8)
+        ]
 
         self.__initialize_thruster_action_servers = [
             ActionServer(
