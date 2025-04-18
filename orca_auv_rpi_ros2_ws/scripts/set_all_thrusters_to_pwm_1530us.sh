36136f5b49719014ceeb4623d95b8a7e39122c9e
DianYueGuo <dianyueguo@gmail.com>
2025-04-18 16:46:33 +0800

    add scripts/set_all_thrusters_to_pwm_1530us.sh

---
+++ orca_auv_rpi_ros2_ws/scripts/set_all_thrusters_to_pwm_1530us.sh
@@ -0,0 +1,8 @@
+ros2 topic pub /orca_auv/thruster_0/set_pwm_output_signal_value_us std_msgs/msg/Int32 "{data: 1530}" --once
+ros2 topic pub /orca_auv/thruster_1/set_pwm_output_signal_value_us std_msgs/msg/Int32 "{data: 1530}" --once
+ros2 topic pub /orca_auv/thruster_2/set_pwm_output_signal_value_us std_msgs/msg/Int32 "{data: 1530}" --once
+ros2 topic pub /orca_auv/thruster_3/set_pwm_output_signal_value_us std_msgs/msg/Int32 "{data: 1530}" --once
+ros2 topic pub /orca_auv/thruster_4/set_pwm_output_signal_value_us std_msgs/msg/Int32 "{data: 1530}" --once
+ros2 topic pub /orca_auv/thruster_5/set_pwm_output_signal_value_us std_msgs/msg/Int32 "{data: 1530}" --once
+ros2 topic pub /orca_auv/thruster_6/set_pwm_output_signal_value_us std_msgs/msg/Int32 "{data: 1530}" --once
+ros2 topic pub /orca_auv/thruster_7/set_pwm_output_signal_value_us std_msgs/msg/Int32 "{data: 1530}" --once
