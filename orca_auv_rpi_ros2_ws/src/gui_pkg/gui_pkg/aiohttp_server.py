eaee8b1c55cce73364bd16adb052c0c5ba539e35
DianYueGuo <dianyueguo@gmail.com>
2025-06-14 08:32:18 +0800

    create class AIOHTTPServer

--- orca_auv_rpi_ros2_ws/src/gui_pkg/gui_pkg/aiohttp_server.py
+++ orca_auv_rpi_ros2_ws/src/gui_pkg/gui_pkg/aiohttp_server.py
@@ -2,48 +2,49 @@ import aiohttp
 from aiohttp import web
 import asyncio
 
-async def respond_index(request):
-    return web.FileResponse(path="./index.html")
+class AIOHTTPServer:
+    async def respond_index(request):
+        return web.FileResponse(path="./index.html")
 
-async def websocket_handler(request):
-    websocket_response = web.WebSocketResponse(protocols=("protocolOne"))
-    await websocket_response.prepare(request)
+    async def websocket_handler(request):
+        websocket_response = web.WebSocketResponse(protocols=("protocolOne"))
+        await websocket_response.prepare(request)
 
-    async for msg in websocket_response:
-        if msg.type == aiohttp.WSMsgType.TEXT:
-            if msg.data == 'close':
-                await websocket_response.close()
-            else:
-                await websocket_response.send_str(msg.data + '/answer')
-        elif msg.type == aiohttp.WSMsgType.ERROR:
-            print('websocket connection closed with exception %s' %
-                  websocket_response.exception())
+        async for msg in websocket_response:
+            if msg.type == aiohttp.WSMsgType.TEXT:
+                if msg.data == 'close':
+                    await websocket_response.close()
+                else:
+                    await websocket_response.send_str(msg.data + '/answer')
+            elif msg.type == aiohttp.WSMsgType.ERROR:
+                print('websocket connection closed with exception %s' %
+                    websocket_response.exception())
 
-    print('websocket connection closed')
+        print('websocket connection closed')
 
-    return websocket_response
+        return websocket_response
 
-@web.middleware
-async def no_cache_middleware(request, handler):
-    response = await handler(request)
-    response.headers['Cache-Control'] = 'no-store'
-    return response
+    @web.middleware
+    async def no_cache_middleware(request, handler):
+        response = await handler(request)
+        response.headers['Cache-Control'] = 'no-store'
+        return response
 
-async def main():
-    app = web.Application(middlewares=[no_cache_middleware])
-    app.add_routes([web.get('/', respond_index),
+    async def main():
+        app = web.Application(middlewares=[AIOHTTPServer.no_cache_middleware])
+        app.add_routes([web.get('/', AIOHTTPServer.respond_index),
                 web.static('/', "./", show_index=True),
-                web.get('/websocket', websocket_handler)])
+                web.get('/websocket', AIOHTTPServer.websocket_handler)])
 
-    runner = web.AppRunner(app)
-    await runner.setup()
-    site = web.TCPSite(runner, 'localhost', 80)
-    await site.start()
+        runner = web.AppRunner(app)
+        await runner.setup()
+        site = web.TCPSite(runner, 'localhost', 80)
+        await site.start()
 
-    while True:
-        await asyncio.sleep(3600)  # sleep forever
+        while True:
+            await asyncio.sleep(3600)  # sleep forever
 
-    await runner.cleanup()
+        await runner.cleanup()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(AIOHTTPServer.main())
