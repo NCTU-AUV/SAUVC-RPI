0e45964a22ed7ca31ca1b41d39bca0c644545507
DianYueGuo <dianyueguo@gmail.com>
2025-10-10 02:55:51 +0800

    set output sink force direction based on orientation

---
+++ orca_auv_rpi_ros2_ws/src/orca_auv_pose_control_pkg/orca_auv_pose_control_pkg/math_utility/quaternion.py
@@ -0,0 +1,31 @@
+from vector_3d import Vector3D
+
+
+class Quaternion:
+
+    def __init__(self, real_part, i_part, j_part, k_part):
+        self.__real_part = real_part
+        self.__vector_part = Vector3D(i_part, j_part, k_part)
+
+    def __mul__(self, other):
+        real_part = self.__real_part * other.__real_part - self.__vector_part.dot_product(other.__vector_part)
+        vector_part = (
+            self.__vector_part.scalar_multiple(other.__real_part) +
+            other.__vector_part.scalar_multiple(self.__real_part) +
+            self.__vector_part.cross_product(other.__vector_part)
+        )
+
+    @property
+    def inverse(self):
+        denominator = self.__real_part**2 + self.__vector_part.dot_product(self.__vector_part)
+
+        real_part = self.__real_part / denominator
+        i_part = - self.__vector_part.x / denominator
+        j_part = - self.__vector_part.y / denominator
+        k_part = - self.__vector_part.z / denominator
+
+        return Quaternion(real_part, i_part, j_part, k_part)
+
+    @property
+    def vector_part(self):
+        return self.__vector_part
