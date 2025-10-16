c70689b50094b310069e9394a2a083a9fc7a1b86
DianYueGuo <dianyueguo@gmail.com>
2025-10-16 16:16:07 +0800

    add simple_motion_control_interface

---
+++ orca_auv_rpi_ros2_ws/src/gui_pkg/gui_pkg/simple_motion_control_interface.js
@@ -0,0 +1,138 @@
+var simple_motion_control_interface_variale = {
+    constant_sink_force_N: 0,
+    translational_push_force_N: 0,
+    rotational_twist_torque_Nm: 0,
+    current_output_wrench: {
+        force: {
+            x: 0,
+            y: 0,
+            z: 0,
+        },
+        torque: {
+            x: 0,
+            y: 0,
+            z: 0,
+        }
+    }
+};
+
+function update_motion_motion_control_wrench() {
+    var msg = simple_motion_control_interface_variale.current_output_wrench;
+
+    console.log("push_auv", msg);
+
+    websocket.send(JSON.stringify({type: "topic", data: {topic_name: "set_output_wrench_at_center_N_Nm", msg: msg}}));
+}
+
+function set_constant_sink_force_button_onclick() {
+    simple_motion_control_interface_variale.constant_sink_force_N = Number(document.getElementById("constant_sink_force_N_input").value);
+    simple_motion_control_interface_variale.current_output_wrench.force.z = simple_motion_control_interface_variale.constant_sink_force_N;
+
+    update_motion_motion_control_wrench();
+}
+
+function set_translational_push_force_button_onclick() {
+    simple_motion_control_interface_variale.translational_push_force_N = Number(document.getElementById("translational_push_force_N_input").value);
+}
+
+function set_rotational_twist_torque_button_onclick() {
+    simple_motion_control_interface_variale.rotational_twist_torque_Nm = Number(document.getElementById("rotational_twist_torque_Nm_input").value);
+}
+
+const push_down_button = document.getElementById("push_down_button");
+
+push_down_button.addEventListener("mousedown", () => {
+    simple_motion_control_interface_variale.current_output_wrench.force.z =
+        simple_motion_control_interface_variale.constant_sink_force_N + simple_motion_control_interface_variale.translational_push_force_N;
+    update_motion_motion_control_wrench();
+});
+
+push_down_button.addEventListener("mouseup", () => {
+    simple_motion_control_interface_variale.current_output_wrench.force.z = simple_motion_control_interface_variale.constant_sink_force_N;
+    update_motion_motion_control_wrench();
+});
+
+const push_up_button = document.getElementById("push_up_button");
+
+push_up_button.addEventListener("mousedown", () => {
+    simple_motion_control_interface_variale.current_output_wrench.force.z =
+        simple_motion_control_interface_variale.constant_sink_force_N - simple_motion_control_interface_variale.translational_push_force_N;
+    update_motion_motion_control_wrench();
+});
+
+push_up_button.addEventListener("mouseup", () => {
+    simple_motion_control_interface_variale.current_output_wrench.force.z = simple_motion_control_interface_variale.constant_sink_force_N;
+    update_motion_motion_control_wrench();
+});
+
+const push_left_button = document.getElementById("push_left_button");
+
+push_left_button.addEventListener("mousedown", () => {
+    simple_motion_control_interface_variale.current_output_wrench.force.y = - simple_motion_control_interface_variale.translational_push_force_N;
+    update_motion_motion_control_wrench();
+});
+
+push_left_button.addEventListener("mouseup", () => {
+    simple_motion_control_interface_variale.current_output_wrench.force.y = 0;
+    update_motion_motion_control_wrench();
+});
+
+const push_right_button = document.getElementById("push_right_button");
+
+push_right_button.addEventListener("mousedown", () => {
+    simple_motion_control_interface_variale.current_output_wrench.force.y = simple_motion_control_interface_variale.translational_push_force_N;
+    update_motion_motion_control_wrench();
+});
+
+push_right_button.addEventListener("mouseup", () => {
+    simple_motion_control_interface_variale.current_output_wrench.force.y = 0;
+    update_motion_motion_control_wrench();
+});
+
+const push_backward_button = document.getElementById("push_backward_button");
+
+push_backward_button.addEventListener("mousedown", () => {
+    simple_motion_control_interface_variale.current_output_wrench.force.x = - simple_motion_control_interface_variale.translational_push_force_N;
+    update_motion_motion_control_wrench();
+});
+
+push_backward_button.addEventListener("mouseup", () => {
+    simple_motion_control_interface_variale.current_output_wrench.force.x = 0;
+    update_motion_motion_control_wrench();
+});
+
+const push_forward_button = document.getElementById("push_forward_button");
+
+push_forward_button.addEventListener("mousedown", () => {
+    simple_motion_control_interface_variale.current_output_wrench.force.x = simple_motion_control_interface_variale.translational_push_force_N;
+    update_motion_motion_control_wrench();
+});
+
+push_forward_button.addEventListener("mouseup", () => {
+    simple_motion_control_interface_variale.current_output_wrench.force.x = 0;
+    update_motion_motion_control_wrench();
+});
+
+const twist_rightward_button = document.getElementById("twist_rightward_button");
+
+twist_rightward_button.addEventListener("mousedown", () => {
+    simple_motion_control_interface_variale.current_output_wrench.torque.z = simple_motion_control_interface_variale.rotational_twist_torque_Nm;
+    update_motion_motion_control_wrench();
+});
+
+twist_rightward_button.addEventListener("mouseup", () => {
+    simple_motion_control_interface_variale.current_output_wrench.torque.z = 0;
+    update_motion_motion_control_wrench();
+});
+
+const twist_leftward_button = document.getElementById("twist_leftward_button");
+
+twist_leftward_button.addEventListener("mousedown", () => {
+    simple_motion_control_interface_variale.current_output_wrench.torque.z = - simple_motion_control_interface_variale.rotational_twist_torque_Nm;
+    update_motion_motion_control_wrench();
+});
+
+twist_leftward_button.addEventListener("mouseup", () => {
+    simple_motion_control_interface_variale.current_output_wrench.torque.z = 0;
+    update_motion_motion_control_wrench();
+});
