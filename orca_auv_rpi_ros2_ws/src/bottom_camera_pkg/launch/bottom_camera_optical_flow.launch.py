632b833fc702a8bbe8795331f687790ca672a8fd
DianYueGuo <dianyueguo@gmail.com>
2025-12-26 14:13:52 +0800

    Show debug image

--- orca_auv_rpi_ros2_ws/src/bottom_camera_pkg/launch/bottom_camera_optical_flow.launch.py
+++ orca_auv_rpi_ros2_ws/src/bottom_camera_pkg/launch/bottom_camera_optical_flow.launch.py
@@ -22,6 +22,7 @@ def generate_launch_description():
             package='bottom_camera_pkg',
             executable='lk_total_transform_node',
             name='lk_total_transform_node',
+            parameters=[{'publish_debug_image': publish_debug_image}],
         ),
         Node(
             package='bottom_camera_pkg',
