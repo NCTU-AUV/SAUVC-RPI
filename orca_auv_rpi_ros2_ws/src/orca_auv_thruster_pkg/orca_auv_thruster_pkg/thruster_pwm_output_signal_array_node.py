508194b20b0050e871f889431be79f0e77d9596c
DianYueGuo <dianyueguo@gmail.com>
2026-02-10 19:06:02 +0800

    Revert "Add publish rate cap for set_pwm_output_signal_value_us topic"
    This reverts commit 9993be290c72ff29d5dc53bb59efb0c9b3cefc56.

--- orca_auv_rpi_ros2_ws/src/orca_auv_thruster_pkg/orca_auv_thruster_pkg/thruster_pwm_output_signal_array_node.py
+++ orca_auv_rpi_ros2_ws/src/orca_auv_thruster_pkg/orca_auv_thruster_pkg/thruster_pwm_output_signal_array_node.py
@@ -11,13 +11,7 @@ class ThrusterPWMOutputSignalArrayNode(Node):
 
         self._thruster_count = 8
         initial_pwm_us = int(self.declare_parameter("initial_pwm_output_signal_value_us", 1500).value)
-        publish_rate_hz = float(self.declare_parameter(
-            "set_pwm_output_signal_value_us_publish_rate_hz",
-            30.0
-        ).value)
         self._pwm_output_signal_value_us = [initial_pwm_us for _ in range(self._thruster_count)]
-        self._publish_dirty = False
-        self._publish_timer = None
 
         self._publisher = self.create_publisher(
             msg_type=Int32MultiArray,
@@ -35,21 +29,8 @@ class ThrusterPWMOutputSignalArrayNode(Node):
             for thruster_number in range(self._thruster_count)
         ]
 
-        if publish_rate_hz > 0.0:
-            period_s = 1.0 / max(publish_rate_hz, 1e-3)
-            self._publish_timer = self.create_timer(period_s, self._publish_timer_callback)
-
     def _set_pwm_output_signal_value_callback(self, msg, thruster_number):
         self._pwm_output_signal_value_us[thruster_number] = msg.data
-        if self._publish_timer is None:
-            self._publish_pwm_array()
-        else:
-            self._publish_dirty = True
-
-    def _publish_timer_callback(self):
-        if not self._publish_dirty:
-            return
-        self._publish_dirty = False
         self._publish_pwm_array()
 
     def _publish_pwm_array(self):
